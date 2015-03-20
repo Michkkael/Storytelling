4ef00fc6565f5397170f8f001f35b8131991081a
Mickael Chanrion <Michkkael@gmail.com>
2015-03-20 19:31:06 +0100

    Clearfix project

---
+++ script/demo.js
@@ -0,0 +1,56 @@
+$(document).ready(init);
+var pi = Math.PI;
+function init() {
+	/* ========== DRAWING THE PATH AND INITIATING THE PLUGIN ============= */
+
+	$.fn.scrollPath("getPath", {
+        scrollSpeed: 40, // Default is 50
+        rotationSpeed: Math.PI / 15 // Default is Math.PI / 15
+    })
+    	
+    	.moveTo(400, 50, {name: "start"})
+    	.lineTo(400, 1000, {name: "intro"})
+    	.lineTo(400, 1400)
+    	.arc(1000, 1400, 600, pi, 0, true, {rotate: pi})
+    	.lineTo(1600, 1000, {name: "part1"})
+    	.lineTo(1600, -900)
+    	.arc(1000, -900, 600, 0, -pi/2, true, {rotate: 3*pi/2})
+    	.lineTo(400, -1500, {name: "part2"})
+    	.lineTo(-200, -1500)
+    	.arc(-200, -900, 600, -pi/2, pi, true, {rotate: 2*pi})
+    	.lineTo(-800, -600, {name: "part3"})
+    	.lineTo(-800, 1400, {name: "part4"})
+    	.lineTo(-800, 2500)
+    	.arc(-200, 2500, 600, pi, pi/2, true, {rotate: 5*pi/2})
+    	.lineTo(800, 3100, {name: "part5"})
+    	.lineTo(2000, 3100)
+    	.arc(2000, 2500, 600, pi/2, 0, true, {rotate: 6*pi/2})
+    	.lineTo(2600, 1500, {name: "conclusion"})
+
+	// We're done with the path, let's initate the plugin on our wrapper element
+	$(".wrapper").scrollPath({drawPath: true, wrapAround: false});
+
+	// Add scrollTo on click on the navigation anchors
+	$("body").find("a").each(function() {
+		var target = $(this).attr("href").replace("#", "");
+		$(this).click(function(e) {
+			e.preventDefault();
+			
+			// Include the jQuery easing plugin (http://gsgd.co.uk/sandbox/jquery/easing/)
+			// for extra easing functions like the one below
+			$.fn.scrollPath("scrollTo", target, 1000, "easeInOutSine");
+		});
+	});
+
+	/* ===================================================================== */
+
+//	$(".settings .show-path").click(function(e) {
+//		e.preventDefault();
+//		$(".sp-canvas").toggle();
+//	}).toggle(function() {
+//		$(this).text("Hide Path");
+//	}, function() {
+//		$(this).text("Show Path");
+//	});
+
+	}
---
+++ script/script.js
@@ -0,0 +1,47 @@
+function partCtrl($scope) {
+
+    //Initialization
+    $scope.part2State = 'start';
+    $scope.part3State = 'null';
+    $scope.part4State = 'null';
+    $scope.part5State = 'null';
+
+    //Part2
+    $scope.part2 = ['v1', 'v2'];
+
+    $scope.showPart2 = function(version) {
+        $scope.part2State = version;
+        $scope.part3State = version + 'Chosen';
+    }
+    //Part3
+    $scope.part3v1 = ['v1_1', 'v1_2'];
+    $scope.part3v2 = ['v2_1', 'v2_2'];
+
+    $scope.showPart3 = function (version) {
+        $scope.part3State = version;
+        $scope.part4State = version + 'Chosen';
+    }
+    //Part4
+    $scope.part4v1_1 = ['v1_1_1', 'v1_1_2'];
+    $scope.part4v1_2 = ['v1_2_1', 'v1_2_2'];
+    $scope.part4v2_1 = ['v2_1_1', 'v2_1_2'];
+    $scope.part4v2_2 = ['v2_2_1', 'v2_2_2'];
+
+    $scope.showPart4 = function (version) {
+        $scope.part4State = version;
+        $scope.part5State = version + 'Chosen';
+    }
+    //Part5
+    $scope.part5v1_1_1 = ['v1_1_1_1', 'v1_1_1_2'];
+    $scope.part5v1_1_2 = ['v1_1_2_1', 'v1_1_2_2'];
+    $scope.part5v1_2_1 = ['v1_2_1_0'];
+    $scope.part5v1_2_2 = ['v1_2_2_1', 'v1_2_2_2'];
+    $scope.part5v2_1_1 = ['v2_1_1_1', 'v2_1_1_2'];
+    $scope.part5v2_1_2 = ['v2_1_2_1', 'v2_1_2_2'];
+    $scope.part5v2_2_1 = ['v2_2_1_0'];
+    $scope.part5v2_2_2 = ['v2_2_2_1', 'v2_2_2_2'];
+
+    $scope.showPart5 = function (version) {
+        $scope.part5State = version;
+    }
+}
