ee83dede3e29f9a391c8c49461e0b2193b36551e
Mickael Chanrion <Michkkael@gmail.com>
2015-03-30 23:53:48 +0200

    Make your choice by clicking on titles

--- script/angular-script.js
+++ script/angular-script.js
@@ -7,74 +7,77 @@ function partCtrl($scope) {
     $scope.part5State = 'null';
 
     //Part2
-    $scope.part2 = ['v1', 'v2'];
+    $scope.part2 = [
+        {id: 'v1', title: "Tailing"}, 
+        {id: 'v2', title: "The chase"}
+    ];
 
     $scope.showPart2 = function(version) {
         $scope.part2State = version;
         $scope.part3State = version + 'Chosen';
     }
     //Part3
-    $scope.part3v1 = ['v1_1', 'v1_2'];
-    $scope.part3v2 = ['v2_1', 'v2_2'];
+    $scope.part3v1 = [
+        {id: 'v1_1', title: "An endless night"}, 
+        {id: 'v1_2', title: "Chasing answers"}
+    ];
+    $scope.part3v2 = [
+        {id: 'v2_1', title: "A cat and mouse game"}, 
+        {id: 'v2_2', title: "Bad revelations"}
+    ];
 
     $scope.showPart3 = function (version) {
         $scope.part3State = version;
         $scope.part4State = version + 'Chosen';
     }
     //Part4
-    $scope.part4v1_1 = ['v1_1_1', 'v1_1_2'];
-    $scope.part4v1_2 = ['v1_2_1', 'v1_2_2'];
-    $scope.part4v2_1 = ['v2_1_1', 'v2_1_2'];
-    $scope.part4v2_2 = ['v2_2_1', 'v2_2_2'];
+    $scope.part4v1_1 = [
+        {id: 'v1_1_1', title: "Personal reflections"},
+        {id: 'v1_1_2', title: "Troubled past"}
+    ];
+    $scope.part4v1_2 = [
+        {id: 'v1_2_1', title: "The Wedding"},
+        {id: 'v1_2_2', title: "Wrong certainties"}
+    ];
+    $scope.part4v2_1 = [
+        {id: 'v2_1_1', title: "A moonlight shadow"},
+        {id: 'v2_1_2', title: "Calm before the storm"}
+    ];
+    $scope.part4v2_2 = [
+        {id: 'v2_2_1', title: "The archive room"}, 
+        {id: 'v2_2_2', title: "The call"}
+    ];
 
     $scope.showPart4 = function (version) {
         $scope.part4State = version;
         $scope.part5State = version + 'Chosen';
     }
     //Part5
-    $scope.part5v1_1_1 = ['v1_1_1_1', 'v1_1_1_2'];
-    $scope.part5v1_1_2 = ['v1_1_2_1', 'v1_1_2_2'];
-    $scope.part5v1_2_1 = ['v1_2_1_0'];
-    $scope.part5v1_2_2 = ['v1_2_2_1', 'v1_2_2_2'];
-    $scope.part5v2_1_1 = ['v2_1_1_1', 'v2_1_1_2'];
-    $scope.part5v2_1_2 = ['v2_1_2_1', 'v2_1_2_2'];
-    $scope.part5v2_2_1 = ['v2_2_1_0'];
-    $scope.part5v2_2_2 = ['v2_2_2_1', 'v2_2_2_2'];
+    $scope.part5v1_1_1 = [
+        {id: 'v1_1_1_1',title: "Kill two birds with one stone"}, 
+        {id: 'v1_1_1_2',title: "As in the good times"}
+    ];
+    $scope.part5v1_1_2 = [
+        {id: 'v1_1_2_1', title: "The long black car"}, 
+        {id: 'v1_1_2_2', title: "Hard truth"}
+    ];
+    $scope.part5v1_2_1 = [{id: 'v1_2_1_0', title: "From the margins to the mainstream"}];
+    $scope.part5v1_2_2 = [
+        {id: 'v1_2_2_1', title: "Birth of an hero"}, 
+        {id: 'v1_2_2_2', title: "Revelation"}
+    ];
+    $scope.part5v2_1_1 = [{id: 'v2_1_1_0', title: "Bad aiming"}];
+    $scope.part5v2_1_2 = [
+        {id: 'v2_1_2_1', title: "The murderer river"}, 
+        {id: 'v2_1_2_2', title: "It's almost the end"}
+    ];
+    $scope.part5v2_2_1 = [{id: 'v2_2_1_0', title: "50 shades of Blue"}];
+    $scope.part5v2_2_2 = [
+        {id: 'v2_2_2_1', title: "Bloody celebration"}, 
+        {id: 'v2_2_2_2', title: "The double-personality"}
+    ];
 
     $scope.showPart5 = function (version) {
         $scope.part5State = version;
     }
 }
-
-
-/*
-v1 : Tailing
-v2 : The chase
-v1_1 : An endless night
-v1_2 : Chasing anwsers
-v2_1 : A cat and mouse game.
-v2_2 : Bad revelations
-v1_1_1 : Personal reflections
-v1_1_2 : Troubled past 
-v1_2_1 : The Wedding 
-v1_2_2 : False certainties
-v2_1_1 : A moonlight tailjob 
-v2_1_2 : Calm before the Storm
-v2_2_1
-v2_2_2
-v1_1_1_1
-v1_1_1_2
-v1_1_2_1
-v1_1_2_2
-v1_2_1_0
-v1_2_2_1
-v1_2_2_2
-v2_1_1_1
-v2_1_1_2
-v2_1_2_1
-v2_1_2_2
-v2_2_1_0
-v2_2_2_1
-v2_2_2_2
-
-*/
